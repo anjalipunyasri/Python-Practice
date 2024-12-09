b0433e8765428eb7e051ff3129babed6fd0bb8fc
vanat <vanathu.chinnapparaj@diggibyte.com>
2024-12-09 08:31:53 +0530

    Added the reverse string function using slicing technique function

---
+++ ReverseStringUsingSlicing.py
@@ -0,0 +1,7 @@
+# Below code snippet is to reverse the string using slicing
+# Input - Welcome
+# Output - emocleW
+def ReverseStringSlicing(string):
+    string = string[::-1]
+    print(string)
+ReverseStringSlicing("Welcome")
