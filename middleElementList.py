16f0fa9ca7b7d1d9f85155c51cb9d965ea38f7a1
vanat <vanathu.chinnapparaj@diggibyte.com>
2024-12-09 23:10:20 +0530

    Added the code ot find the middle element in the given list

---
+++ middleElementList.py
@@ -0,0 +1,7 @@
+# Function to calculate the middle number in the given list
+# Input - ['2','6','7','9','3']
+# Output - 7
+
+input_list = ['2','6','7','9','3']
+middle_element = int((len(input_list)/2))
+print(input_list[middle_element])
